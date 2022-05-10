2fce1a5bca070e92081b292d0df6f2a221a5635c
Arash Mousavi <amousavi@avidgaming.com>
2022-05-10 10:26:08 +0100

    ale to run fixers on save

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -4,11 +4,17 @@ local nnoremap = h.nnoremap
 
 nnoremap("<Leader>f", ":ALEFix<CR>", { desc = "Fix code style" })
 
+g.ale_lint_on_save = 1
+g.ale_fix_on_save = 1
 g.ale_rust_rls_toolchain = "stable"
 g.ale_ruby_rubocop_auto_correct_all = 1
 g.ale_sign_error = ""
 g.ale_sign_warning = ""
 g.ale_linter_aliases = { vue = { "vue", "javascript", "scss" } }
+g.ale_sign_column_always = 1
+g.ale_rust_cargo_use_clippy = 1
+g.ale_sign_column_always = 1
+g.ale_rust_cargo_use_clippy = 1
 g.ale_fixers = {
   ["*"] = { "prettier", "remove_trailing_lines", "trim_whitespace" },
   ruby = { "rubocop" },
@@ -33,5 +39,3 @@ g.ale_pattern_options = {
   [".min.js$"] = { ale_linters = {}, ale_fixers = {} },
   [".min.css$"] = { ale_linters = {}, ale_fixers = {} },
 }
-g.ale_sign_column_always = 1
-g.ale_rust_cargo_use_clippy = 1
