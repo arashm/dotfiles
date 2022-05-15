fbcdb253cf204c43e5cb763d3d0270b7b7807c86
Arash Mousavi <mousavi.arash@gmail.com>
2022-05-15 23:53:59 +0100

    add a list of default extensions for coc to install

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -1,6 +1,19 @@
 local h = require("helpers")
 local nnoremap = h.nnoremap
 
+-- COC will install these extensions if they are missing.
+-- These are the extensions that I want on every machine I have by default.
+vim.g.coc_global_extensions = {
+  'coc-lists',
+  'coc-css',
+  'coc-json',
+  'coc-html',
+  'coc-yaml',
+  'coc-snippets',
+  'coc-tsserver',
+  'coc-snippets',
+}
+
 vim.cmd([[
 function! s:coc_show_documentation()
   if (index(['vim','help'], &filetype) >= 0)
