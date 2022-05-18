98401f677978e818c18c8633df92a94522db35bd
Arash Mousavi <mousavi.arash@gmail.com>
2022-05-18 02:50:39 +0100

    show virtualtext with ale warnings

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -4,12 +4,15 @@ local nnoremap = h.nnoremap
 
 nnoremap("<Leader>f", ":ALEFix<CR>", { desc = "Fix code style" })
 
+g.ale_disable_lsp = 1
+g.ale_virtualtext_cursor = 1
+g.ale_virtualtext_prefix = "▸ "
 g.ale_lint_on_save = 1
 g.ale_fix_on_save = 1
 g.ale_rust_rls_toolchain = "stable"
 g.ale_ruby_rubocop_auto_correct_all = 1
-g.ale_sign_error = ""
-g.ale_sign_warning = ""
+g.ale_sign_error = "😞"
+g.ale_sign_warning = "😐"
 g.ale_linter_aliases = { vue = { "vue", "javascript", "scss" } }
 g.ale_sign_column_always = 1
 g.ale_rust_cargo_use_clippy = 1
@@ -40,3 +43,11 @@ g.ale_pattern_options = {
   [".min.js$"] = { ale_linters = {}, ale_fixers = {} },
   [".min.css$"] = { ale_linters = {}, ale_fixers = {} },
 }
+
+vim.cmd([[
+highlight link ALEVirtualTextError ErrorMsg
+highlight link ALEVirtualTextStyleError ALEVirtualTextError
+highlight link ALEVirtualTextWarning WarningMsg
+highlight link ALEVirtualTextInfo ALEVirtualTextWarning
+highlight link ALEVirtualTextStyleWarning ALEVirtualTextWarning
+]])
