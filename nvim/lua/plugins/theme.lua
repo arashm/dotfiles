f890e2f884c153021d1f0167d811bef8e812ca9c
Arash Mousavi <mousavi.arash@gmail.com>
2022-05-25 22:17:20 +0100

    add new ruby debugger syntax to highlights

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -10,5 +10,7 @@ hi link SpellRare SpellBad
 
 " Make those debugger statements painfully obvious
 au BufEnter,BufWritePost *.rb syn match error contained "\<binding.pry\>"
+au BufEnter,BufWritePost *.rb syn match error contained "\<binding.break\>"
 au BufEnter,BufWritePost *.rb syn match error contained "\<debugger\>"
+au BufEnter,BufWritePost *.rb syn match error contained "\<binding.b\>"
 ]])
