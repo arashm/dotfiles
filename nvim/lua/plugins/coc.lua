ef746b5c0469d50b3af67aa7e99b59030110d1e5
Arash Mousavi <amousavi@avidgaming.com>
2022-08-05 14:26:54 +0100

    temp mapping for selecting completions

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -1,5 +1,6 @@
 local h = require("helpers")
 local nnoremap = h.nnoremap
+local inoremap = h.inoremap
 
 -- COC will install these extensions if they are missing.
 -- These are the extensions that I want on every machine I have by default.
@@ -18,3 +19,8 @@ nnoremap("gd", "<Plug>(coc-definition)")
 nnoremap("gy", "<Plug>(coc-type-definition)")
 nnoremap("gi", "<Plug>(coc-implementation)")
 nnoremap("gr", "<Plug>(coc-references)")
+
+vim.cmd([[
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+]])
