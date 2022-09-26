aacc10e5e61d058d9176fb5ba7f87737b5b00bfd
Arash Mousavi <mousavi.arash@gmail.com>
2022-09-26 01:02:47 +0100

    Add vim-illuminate and vim-flaterm

---
+++ nvim/lua/plugins/floaterm.lua
@@ -0,0 +1,6 @@
+vim.g.floaterm_shell = "fish"
+vim.g.floaterm_keymap_new = "<F7>"
+vim.g.floaterm_keymap_prev = "<F8>"
+vim.g.floaterm_keymap_next = "<F9>"
+vim.g.floaterm_keymap_toggle = "<F12>"
+vim.g.floaterm_title = "Terminal: $1/$2"
