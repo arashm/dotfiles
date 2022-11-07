0ddecd8def0d55acd472362e7d7d1f312f196e27
Arash Mousavi <amousavi@avidgaming.com>
2022-11-07 18:35:11 +0000

    Add mapping to grep ruby and js files specifically

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,7 +23,36 @@ nnoremap("<leader>gl", "<cmd>Telescope git_commits<cr>")
 nnoremap("<leader>gc", "<cmd>Telescope git_branches<cr>")
 nnoremap("<leader>z", "<cmd>Telescope coc document_symbols<cr>")
 nnoremap("<leader>gg", "<cmd>Telescope grep_string<cr>")
+
 vnoremap("<leader>gg", function()
   local text = h.getVisualSelection()
-  tb.live_grep({ default_text = text })
+  tb.live_grep({ default_text = text, prompt_title = "Find word:" .. text })
+end)
+
+nnoremap("<leader>gr", function()
+  tb.live_grep({ type_filter = "ruby", prompt_title = "Ruby Grep" })
+end)
+
+vnoremap("<leader>gr", function()
+  local text = h.getVisualSelection()
+  tb.live_grep({ default_text = text, type_filter = "ruby", prompt_title = "Ruby Grep: " .. text })
+end)
+
+nnoremap("<leader>grr", function()
+  local text = vim.call("expand", "<cword>")
+  tb.live_grep({ default_text = text, type_filter = "ruby", prompt_title = "Ruby Grep: " .. text })
+end)
+
+nnoremap("<leader>gj", function()
+  tb.live_grep({ type_filter = "js", prompt_title = "JS Grep" })
+end)
+
+vnoremap("<leader>gj", function()
+  local text = h.getVisualSelection()
+  tb.live_grep({ default_text = text, type_filter = "js", prompt_title = "JS Grep: " .. text })
+end)
+
+nnoremap("<leader>gjj", function()
+  local text = vim.call("expand", "<cword>")
+  tb.live_grep({ default_text = text, type_filter = "js", prompt_title = "JS Grep: " .. text })
 end)
