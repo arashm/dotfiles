de2ec35d908848b37819e33f925d7d3dde297052
Arash Mousavi <amousavi@avidgaming.com>
2022-11-11 14:13:21 +0000

    fix git conflict markers' highlights not showing up

--- nvim/lua/plugins/conflict_marker.lua
+++ nvim/lua/plugins/conflict_marker.lua
@@ -5,8 +5,9 @@ g.conflict_marker_begin = "^<<<<<<< .*$"
 g.conflict_marker_end = "^>>>>>>> .*$"
 
 vim.cmd([[
-  highlight ConflictMarkerBegin ctermbg=29
-  highlight ConflictMarkerOurs ctermbg=23
-  highlight ConflictMarkerTheirs ctermbg=24
-  highlight ConflictMarkerEnd ctermbg=25
+  highlight ConflictMarkerBegin guibg=#2f7366
+  highlight ConflictMarkerOurs guibg=#2e5049
+  highlight ConflictMarkerTheirs guibg=#344f69
+  highlight ConflictMarkerEnd guibg=#2f628e
+  highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
 ]])
