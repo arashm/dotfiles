2919777a00a4ffb70a86e4bd1ef4cf79cfbf4929
Arash Mousavi <amousavi@avidgaming.com>
2022-12-06 11:16:56 +0000

    fix linter issue with quotations

--- nvim/lua/plugins/vim_test.lua
+++ nvim/lua/plugins/vim_test.lua
@@ -3,7 +3,7 @@ local nnoremap = h.nnoremap
 
 vim.g["test#strategy"] = "vtr"
 
-nnoremap("<Leader>t", ":TestFile<CR>", { desc = 'Run the tests of the whole file' })
-nnoremap("<Leader>s", ":TestNearest<CR>", { desc = 'Run the nearest test to the cursor' })
-nnoremap("<Leader>a", ":TestSuite<CR>", { desc = 'Run the whole test suite' })
-nnoremap("<Leader>l", ":TestLast<CR>", { desc = 'Re-run the last test command issued' })
+nnoremap("<Leader>t", ":TestFile<CR>", { desc = "Run the tests of the whole file" })
+nnoremap("<Leader>s", ":TestNearest<CR>", { desc = "Run the nearest test to the cursor" })
+nnoremap("<Leader>a", ":TestSuite<CR>", { desc = "Run the whole test suite" })
+nnoremap("<Leader>l", ":TestLast<CR>", { desc = "Re-run the last test command issued" })
