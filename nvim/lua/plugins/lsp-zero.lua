03c1cf35f6b92ac2baf60284d82274baf9c65fe8
Arash Mousavi <mousavi.arash@gmail.com>
2023-01-06 16:18:01 +0000

    Install and configure nvim lsp using lsp-zero

---
+++ nvim/lua/plugins/lsp-zero.lua
@@ -0,0 +1,48 @@
+local lsp = require("lsp-zero")
+local null_ls = require("null-ls")
+lsp.preset("recommended")
+
+-- Install these servers by default
+lsp.ensure_installed({
+  "tsserver",
+  "eslint",
+  "sumneko_lua",
+  "solargraph",
+})
+
+null_ls.setup({
+  diagnostics_format = "[#{c}] #{m} (#{s})",
+  on_attach = require("lsp-format").on_attach,
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.rubocop.with({
+      command = "bundle",
+      args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
+    }),
+    null_ls.builtins.formatting.rubocop.with({
+      command = "bundle",
+      args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
+    }),
+    null_ls.builtins.diagnostics.eslint,
+  },
+})
+
+lsp.nvim_workspace()
+lsp.setup()
+require("fidget").setup({})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,7 +11,6 @@ require("telescope").setup({
     cache_picker = { num_pickers = 20 },
   },
 })
-require("telescope").load_extension("coc")
 
 nnoremap("<c-p>", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>p", "<cmd>Telescope pickers<CR>")
@@ -21,7 +20,7 @@ nnoremap("<leader>b", "<cmd>Telescope buffers<cr>")
 nnoremap("<leader>gs", "<cmd>Telescope git_status<cr>")
 nnoremap("<leader>gl", "<cmd>Telescope git_commits<cr>")
 nnoremap("<leader>gc", "<cmd>Telescope git_branches<cr>")
-nnoremap("<leader>z", "<cmd>Telescope coc document_symbols<cr>")
+nnoremap("<leader>z", "<cmd>Telescope lsp_document_symbols<cr>")
 nnoremap("<leader>gg", "<cmd>Telescope grep_string<cr>")
 
 vnoremap("<leader>gg", function()
