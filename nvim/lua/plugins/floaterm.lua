b8f4f0ef923e9f79b7a3776ea49bf5b166ee287d
Arash Mousavi <me@amousavi.dev>
2024-05-19 21:32:39 +0100

    Add leader+G to open lazygit inside the floaterm overlay

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -1,6 +1,11 @@
+local h = require("helpers")
+local nnoremap = h.nnoremap
+
 vim.g.floaterm_shell = "fish"
 vim.g.floaterm_keymap_new = "<F7>"
 vim.g.floaterm_keymap_prev = "<F8>"
 vim.g.floaterm_keymap_next = "<F9>"
 vim.g.floaterm_keymap_toggle = "<F12>"
 vim.g.floaterm_title = "Terminal: $1/$2"
+
+nnoremap("<leader>G", "<cmd>FloatermNew lazygit<CR>")
