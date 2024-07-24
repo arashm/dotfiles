82e808ac8feff05c3ad594a00f554716ceed356d
Arash Mousavi <me@amousavi.dev>
2024-07-24 19:32:41 +0100

    Add aerial plugin

--- nvim/lua/plugins/vim_test.lua
+++ nvim/lua/plugins/vim_test.lua
@@ -5,5 +5,5 @@ vim.g["test#strategy"] = "vtr"
 
 nnoremap("<Leader>t", ":TestFile<CR>", { desc = "Run the tests of the whole file" })
 nnoremap("<Leader>s", ":TestNearest<CR>", { desc = "Run the nearest test to the cursor" })
-nnoremap("<Leader>a", ":TestSuite<CR>", { desc = "Run the whole test suite" })
+-- nnoremap("<Leader>a", ":TestSuite<CR>", { desc = "Run the whole test suite" })
 nnoremap("<Leader>l", ":TestLast<CR>", { desc = "Re-run the last test command issued" })
