64d90be5c2da8bf39282722e6283dcb52b8d91df
Arash Mousavi <me@amousavi.dev>
2026-04-10 11:16:15 +0100

    Remove CodeCompanion plugin

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,20 +1,20 @@
-require("codecompanion").setup({
-  strategies = {
-    chat = {
-      adapter = "copilot",
-      model = "claude-sonnet-4.5",
-    },
-  },
-  extensions = {
-    mcphub = {
-      callback = "mcphub.extensions.codecompanion",
-      opts = {
-        make_vars = true,
-        make_slash_commands = true,
-        show_result_in_chat = true,
-      },
-    },
-  },
-})
-
-vim.keymap.set({ "n", "v" }, "<Leader>c", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+-- require("codecompanion").setup({
+--   strategies = {
+--     chat = {
+--       adapter = "copilot",
+--       model = "claude-sonnet-4.5",
+--     },
+--   },
+--   extensions = {
+--     mcphub = {
+--       callback = "mcphub.extensions.codecompanion",
+--       opts = {
+--         make_vars = true,
+--         make_slash_commands = true,
+--         show_result_in_chat = true,
+--       },
+--     },
+--   },
+-- })
+--
+-- vim.keymap.set({ "n", "v" }, "<Leader>c", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
